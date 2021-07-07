fd4b56b10c640ba07fc18cbfa32b9e6459775475
Vasco-Win <kravchuk.vasilii@yandex.ru>
2021-07-08 00:08:30 +0300

    Created csp.

---
+++ Lesson3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Автор - Кравчук Василий
+/// </summary>
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// По нажатию Enter очищает экран
+        /// </summary>
+        static void next()
+        {
+            Console.Write("\nДля продолжения нажмите Enter..");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
