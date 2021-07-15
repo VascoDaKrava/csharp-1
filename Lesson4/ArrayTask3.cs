ce039ce247c2d563caec883e63f6f342def7ab59
Vasco-Win <kravchuk.vasilii@yandex.ru>
2021-07-15 05:44:42 +0300

    Job s done

---
+++ Lesson4/ArrayTask3.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Автор - Кравчук Василий
+/// </summary>
+namespace Lesson4
+{
+    /// <summary>
+    /// Класс третьего задания о массивах
+    /// </summary>
+    class ArrayTask3
+    {
+        #region Fields
+        private int[] arrayInt;
+        #endregion
+
+        #region Constructors
+        public ArrayTask3(int[] arr)
+        {
+            arrayInt = arr;
+        }
+
+        /// <summary>
+        /// Конструктор, создающий массив размера length и заполняющий числами от начального значения startNumber с заданным шагом step.
+        /// </summary>
+        /// <param name="length"></param>
+        /// <param name="startNumber"></param>
+        /// <param name="step"></param>
+        public ArrayTask3(int length, int startNumber, int step)
+        {
+            arrayInt = new int[length];
+            arrayInt[0] = startNumber;
+
+            for (int i = 1; i < length; i++)
+            {
+                arrayInt[i] = arrayInt[i - 1] + step;
+            }
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Сумма элементов массива
+        /// </summary>
+        public int sum
+        {
+            get
+            {
+                int sum = 0;
+                for (int i = 0; i < arrayInt.Length; i++)
+                {
+                    sum += arrayInt[i];
+                }
+                return sum;
+            }
+        }
+
+        /// <summary>
+        /// Количество максимальных элементов
+        /// </summary>
+        public int MaxCount
+        {
+            get
+            {
+                int[] arraySorted = new int[arrayInt.Length];
+                int i = arraySorted.Length - 1;
+
+                Array.Copy(arrayInt, arraySorted, arrayInt.Length);
+                Array.Sort(arraySorted);
+
+                while (i > 0 && arraySorted[i] == arraySorted[i - 1])
+                {
+                    i--;
+                }
+
+                return arraySorted.Length - i;
+            }
+        }
+
+        /// <summary>
+        /// Доступ к элементам массива по индексам
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public int this[int index]
+        {
+            get
+            {
+                return arrayInt[index];
+            }
+            set
+            {
+                arrayInt[index] = value;
+            }
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            string result = "Element\tValue\n";
+            for (int i = 0; i < arrayInt.Length; i++)
+            {
+                result += $"{i + 1:D2}\t{arrayInt[i]}\n";
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Возвращает новый массив с измененными знаками у всех элементов входного массива
+        /// </summary>
+        /// <returns></returns>
+        public int[] Inverse()
+        {
+            int[] arrayInverse = new int[arrayInt.Length];
+            for (int i = 0; i < arrayInt.Length; i++)
+            {
+                arrayInverse[i] = -arrayInt[i];
+            }
+            return arrayInverse;
+        }
+
+        /// <summary>
+        /// Умножение каждого элемента массива на заданное число
+        /// </summary>
+        /// <param name="a"></param>
+        public void Multi(int a)
+        {
+            for (int i = 0; i < arrayInt.Length; i++)
+            {
+                arrayInt[i] *= a;
+            }
+        }
+
+        /// <summary>
+        /// Частотный массив
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<int, int> frequencyDictionary()
+        {
+            //int count = 1;
+            Array.Sort(arrayInt);
+            Dictionary<int, int> result = new Dictionary<int, int>();
+            result.Add(arrayInt[0], 1);
+
+            for (int i = 1; i < arrayInt.Length; i++)
+			{
+                if (arrayInt[i] == arrayInt[i-1])
+                {
+                    result[arrayInt[i]]++;
+                }
+                else
+                {
+                    result.Add(arrayInt[i], 1);
+                }
+			}
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ Lesson4/ArrayTask5.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.IO;
+/// <summary>
+/// Автор - Кравчук Василий
+/// </summary>
+namespace Lesson4
+{
+    /// <summary>
+    /// Класс для работы с двумерными массивами
+    /// </summary>
+    class ArrayTask5
+    {
+        #region Fields
+        private int[,] arr;
+        #endregion
+
+        #region Constructors
+        public ArrayTask5(int[,] arr)
+        {
+            this.arr = arr;
+        }
+
+        /// <summary>
+        /// Конструктор, заполняющий массив случайными числами
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public ArrayTask5(int x, int y, int minA, int maxA)
+        {
+            arr = new int[x, y];
+            Random r = new Random();
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    arr[i, j] = r.Next(minA, maxA + 1);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Конструктор, заполняющий массив из файла path, элементы разделены separator
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="separator"></param>
+        public ArrayTask5(string path, char separator)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException($"Файл {path} не найден.");
+
+            StreamReader sr = new StreamReader(path);
+            string[] forX = File.ReadAllLines(path);
+            sr.Close();
+
+            string[] forY = forX[0].Split(separator);
+
+            arr = new int[forX.Length, forY.Length];
+
+            for (int i = 0; i < forX.Length; i++)
+            {
+                forY = forX[i].Split(separator);
+                for (int j = 0; j < forY.Length; j++)
+                {
+                    arr[i, j] = int.Parse(forY[j]);
+                }
+            }
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Поиск минимального значения
+        /// </summary>
+        public int min
+        {
+            get
+            {
+                int res = arr[0, 0];
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        if (arr[i, j] < res)
+                        {
+                            res = arr[i, j];
+                        }
+                    }
+                }
+                return res;
+            }
+        }
+
+        /// <summary>
+        /// Поиск максимального значения
+        /// </summary>
+        public int max
+        {
+            get
+            {
+                int res = arr[0, 0];
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        if (arr[i, j] > res)
+                        {
+                            res = arr[i, j];
+                        }
+                    }
+                }
+                return res;
+            }
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Сумма всех элементов
+        /// </summary>
+        /// <returns></returns>
+        public int sum()
+        {
+            int res = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    res += arr[i, j];
+                }
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// Сумма всех элементов массива больше заданного
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public int sum(int a)
+        {
+            int res = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] > a)
+                    {
+                        res += arr[i, j];
+                    }
+                }
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// Метод, возвращающий номер максимального элемента массива (через параметры, используя модификатор ref или out).
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public void whereMax(out int x, out int y)
+        {
+            x = 0;
+            y = 0;
+            int max = arr[x, y];
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] > max)
+                    {
+                        max = arr[i, j];
+                        x = i;
+                        y = j;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Вывод массива
+        /// </summary>
+        public void Print()
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Вывод массива в файл
+        /// </summary>
+        /// <param name="path"></param>
+        public void Print(string path)
+        {
+            if (File.Exists(path))
+                throw new IOException($"Такой файл ({path}) уже существует.");
+            StreamWriter sw = new StreamWriter(path);
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    sw.Write(arr[i, j] + "\t");
+                }
+                sw.WriteLine();
+            }
+            sw.Close();
+        }
+        #endregion
+    }
+}
