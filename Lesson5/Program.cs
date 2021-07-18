95d5521e5a5ac3628318197c8506c9d609f0307e
Vasco-Win <kravchuk.vasilii@yandex.ru>
2021-07-18 21:19:48 +0300

    Task 2 and 3 was done.

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -14,7 +14,7 @@ namespace Lesson5
     {
         static void Main(string[] args)
         {
-            //goto Label;
+            goto Label;
             #region Задание 1. Корректность ввода логина.
             // 1. Создать программу, которая будет проверять корректность ввода логина.
             //    Корректным логином будет строка от 2 до 10 символов, содержащая только буквы латинского алфавита или цифры,
@@ -59,10 +59,9 @@ namespace Lesson5
             }
 
             next();
-        #endregion
+            #endregion
 
-        Label:;
-            #region MyRegion
+            #region Задание 2. Класс Message для обработки текста.
             // 2. Разработать статический класс Message, содержащий следующие статические методы для обработки текста:
             // а) Вывести только те слова сообщения, которые содержат не более n букв.
             // б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
@@ -72,15 +71,58 @@ namespace Lesson5
             //    В качестве параметра в него передается массив слов и текст,
             //    в качестве результата метод возвращает сколько раз каждое из слов массива входит в этот текст.
             //    Здесь требуется использовать класс Dictionary.
-            #endregion
+            Console.WriteLine("Задание 2. Класс Message для обработки текста.\n");
 
-            #region MyRegion
+            int max_n = 5;
+            char endLetter = 'е';
+            string[] arrayForFreq = new string[] { "Москве", "болт", "COVID-19" };
+            Dictionary<string, int> freqWords = new Dictionary<string, int>();
+
+            string str_task2 = @"Сейчас в СМИ в Москве
+Прокуратура требует ужесточить обвинение девушке, сбившей детей в Москве
+Руководство «Команды 29» адвоката Павлова приняло решение о самороспуске
+В Сколтехе обнаружили трудноизлечимый «длинный COVID-19»
+МВД Украины сообщило о нападении на пограничников на участке границы с Россией
+Абстрагирование...";
+
+            Console.WriteLine($"Исходная строка : \n\n{str_task2}\n");
+
+            Console.WriteLine($"\nСписок слов, которые содержат не более {max_n} букв :");
+            Message.NoMoreLetters(str_task2, max_n);
+
+            Console.WriteLine($"\nИз текста удалены все слова, оканчивающиеся на {endLetter} : \n" +
+                $"{Message.DeleteSomeWords(str_task2, endLetter)}\n");
+
+            Console.WriteLine($"Самое длинное слово в тексте : {Message.LongestWord(str_task2)}\n");
+
+            Console.WriteLine($"Строка из самых длинных слов : \"{Message.AllLongestWords(str_task2)}\"\n");
+
+            Console.WriteLine("Частотный анализ текста :");
+            freqWords = Message.FrequencyOfWords(arrayForFreq, str_task2);
+            foreach (var item in freqWords)
+            {
+                Console.WriteLine(item.Key + " - " + item.Value);
+            }
+            next();
+        #endregion
+
+            #region Задание 3. Опредение перестановки.
             // 3. * Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой.
             //    Например:
             //    badc являются перестановкой abcd.
-            #endregion
+            Console.WriteLine("Задание 3. Опредение перестановки.\n");
+            string a = "qwe";
+            string b = "ewq";
+            string c = "ewa";
 
-            #region MyRegion
+            Console.WriteLine($"Строка {a} является перестановкой {b}? - {transposition(a, b)}");
+            Console.WriteLine($"Строка {a} является перестановкой {c}? - {transposition(a, c)}");
+
+            next();
+        #endregion
+
+        Label:;
+            #region Задание 4. Три худших ученика.
             // 4. * Задача ЕГЭ.
             //    На вход программе подаются сведения о сдаче экзаменов учениками 9 - х классов некоторой средней школы.
             //    В первой строке сообщается количество учеников N, которое не меньше 10, но не превосходит 100,
@@ -95,8 +137,26 @@ namespace Lesson5
             //    Требуется написать как можно более эффективную программу, которая будет выводить на экран фамилии и
             //    имена трёх худших по среднему баллу учеников. Если среди остальных есть ученики,
             //    набравшие тот же средний балл, что и один из трёх худших, следует вывести и их фамилии и имена.
+            Console.WriteLine("Задание 4. Три худших ученика.\n");
+            
             #endregion
 
+        }
+
+        /// <summary>
+        /// Определение, является ли одна строка перестановкой другой
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        static bool transposition(string a, string b)
+        {
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                if (a[i] != b[a.Length - i - 1])
+                    return false;
+            }
+            return true;
         }
 
         /// <summary>
@@ -111,11 +171,11 @@ namespace Lesson5
     }
 
     /// <summary>
-    /// Класс для второго задания
+    /// Класс для работы с текстом второго задания
     /// </summary>
     static class Message
     {
-        static string[] separators = { ",", ".", "!", "?", ";", ":", " " };
+        static string[] separators = { ",", ".", "!", "?", ";", ":", " ", "«", "»", "\n", ((char)13).ToString() };
 
         #region Methods
 
@@ -135,29 +195,95 @@ namespace Lesson5
             }
         }
 
-        // б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
-        
-        public static string DeleteSomeWorlds(string str, char a)
+        /// <summary>
+        /// Удаление из сообщения всех слов, которые заканчиваются на заданный символ
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static string DeleteSomeWords(string str, char a)
         {
             string[] words = str.Split(separators, StringSplitOptions.RemoveEmptyEntries);
 
             foreach (string item in words)
             {
                 if (item[item.Length - 1] == a)
-            //str.Replace()
-
+                    str = str.Replace(item, null);
             }
+
+            return str;
         }
 
 
-        // в) Найти самое длинное слово сообщения.
+        /// <summary>
+        /// Поиск самого длинного слова в тексте
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string LongestWord(string str)
+        {
+            string[] words = str.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            int maxLength = 0;
+            string longest = "";
 
-        // г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+            foreach (string item in words)
+            {
+                if (item.Length > maxLength)
+                {
+                    maxLength = item.Length;
+                    longest = item;
+                }
+            }
+            return longest;
+        }
 
-        // д) *** Создать метод, который производит частотный анализ текста.
-        //    В качестве параметра в него передается массив слов и текст,
-        //    в качестве результата метод возвращает сколько раз каждое из слов массива входит в этот текст.
-        //    Здесь требуется использовать класс Dictionary.
+        /// <summary>
+        /// Строка из самых длинных слов, сформированная из StringBuilder
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string AllLongestWords(string str)
+        {
+            string[] words = str.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            int maxLength = LongestWord(str).Length;
+            StringBuilder result = new StringBuilder();
+
+            foreach (string item in words)
+            {
+                if (item.Length == maxLength)
+                {
+                    result.Append(item);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// Частотный анализ текста
+        /// </summary>
+        /// <param name="arrayOfWords">Массив слов</param>
+        /// <param name="text">Текст для анализа</param>
+        /// <returns></returns>
+        public static Dictionary<string, int> FrequencyOfWords(string[] arrayOfWords, string text)
+        {
+            string[] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> result = new Dictionary<string, int>();
+            int count;
+
+            for (int i = 0; i < arrayOfWords.Length; i++)
+            {
+                count = 0;
+                foreach (string item in words)
+                {
+                    if (item == arrayOfWords[i])
+                        count++;
+                }
+                result.Add(arrayOfWords[i], count);
+            }
+
+            return result;
+        }
         #endregion
     }
 }
